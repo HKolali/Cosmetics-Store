7fc3ee2131e1ab42c7a98ae91b05b525ffa10186
Hossein Kolali <kolali.hossein@gmail.com>
2024-01-21 16:32:06 +0330

    complete main menu

--- src/Component/Menu/Menu.js
+++ src/Component/Menu/Menu.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './Menu.css'
 import MenuItem from './MenuItem/MenuItem'
-import { json } from 'react-router-dom'
+import { CiUser, CiShoppingCart } from "react-icons/ci";
 
 export default function Menu() {
 
@@ -9,18 +9,30 @@ export default function Menu() {
 
   useEffect(() => {
     fetch('menu.json')
-    .then((response) => {
-      return response.json();
-    }).then((data) => {
-      setMenu(data)
-    })
+      .then((response) => {
+        return response.json();
+      }).then((data) => {
+        setMenu(data)
+      })
   }, [])
 
   return (
     <div className='menu'>
-      {menu.map(menuItem => (
-        <MenuItem key={menuItem.id} {...menuItem}/>
-      ))}
+      <div className="menu__parent-item left-menu">
+        {menu.map(menuItem => (
+          menuItem.direction == "left" && (<MenuItem key={menuItem.id} {...menuItem} />)
+        ))}
+        <CiShoppingCart className='menu__icon'/>
+        <CiUser className='menu__icon'/>
+      </div>
+      <div className="menu__parent-item">
+        <img className='menu__logo' src="image/logo/logo.jpg" alt="" width={80} />
+      </div>
+      <div className="menu__parent-item right-menu">
+        {menu.map(menuItem => (
+          menuItem.direction == "right" && (<MenuItem key={menuItem.id} {...menuItem} />)
+        ))}
+      </div>
     </div>
   )
 }
--- src/Component/Menu/MenuItem/MenuItem.js
+++ src/Component/Menu/MenuItem/MenuItem.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import './MenuItem.css'
+import { SlArrowDown } from "react-icons/sl";
 
 export default function MenuItem(props) {
+
+  useEffect(() => {
+    {console.log("==>",props.submenu)}
+  })
   return (
     <>
+      {props.submenu.length > 0 ?
+        (<>
+          <Link className='menu-item submenu' to={props.link}>{props.title}
+          <ul className='sub-menu-list'>
+            {props.submenu.map(item => (<li><Link className='sub-menu-list-item' to={item.link}>{item.title}</Link></li>))}
+          </ul>
+          </Link>
+          <SlArrowDown className='menu-item-dropdown'/>
+          
+        </>
+        )
+        : (<>
         <Link className='menu-item' to={props.link}>{props.title}</Link>
+        </>
+        )
+      }
     </>
   )
 }
